671d2d1fca4fe44a8cb17e2db2c73f60f9677ced
Cix8 <francesco.natal_fn@libero.it>
2022-10-05 16:12:36 +0200

    add PriceValidation, add defaultValidations in PizzaModel, add if statments for img_src in Pizza_Views

--- Models/PizzaModel.cs
+++ Models/PizzaModel.cs
@@ -1,18 +1,28 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using la_mia_pizzeria_static.MyValidations;
 
 namespace la_mia_pizzeria_static.Models
 {
     public class PizzaModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [StringLength(30, ErrorMessage = "Il nome non può avere più di 30 caratteri")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
         [Column(TypeName = "text")]
         public string Description { get; set; }
 
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
         [Column(TypeName = "text")]
         public string Image { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [PriceValidation]
         public float Price { get; set; }
 
         public PizzaModel()
---
+++ MyValidations/PriceValidation.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace la_mia_pizzeria_static.MyValidations
+{
+    public class PriceValidation : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            float fieldValue = (float)value;
+
+            if(fieldValue <= 0)
+            {
+                return new ValidationResult("Il prezzo non può essere negativo o uguale a zero");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
